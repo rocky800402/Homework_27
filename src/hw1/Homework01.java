9057eb309a00a4902e2102740f58225a2f0118e7
Rocky <rocky800402@gmail.com>
2023-10-30 16:39:26 +0800

    更新HW1

--- src/hw1/Homework01.java
+++ src/hw1/Homework01.java
@@ -5,7 +5,7 @@ public class Homework01 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		test01();
-		System.out.println("--------------------");
+		System.out.println("------------------------");
 		test02();
 		System.out.println("--------------------");
 		test03();
